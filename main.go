f6528cc73dd40161fd1cdeadf123efa98be4d9c6
liqingshan <15054854614@163.com>
2024-05-31 16:18:58 +0800

    1

--- main.go
+++ main.go
@@ -8,4 +8,5 @@ func main() {
 	name := "Go Developers"
 	fmt.Println("Azure for", name)
 	initializeRfid()
+	stopRfid()
 }
--- rfid.go
+++ rfid.go
@@ -7,15 +7,33 @@ import (
 	"go.bug.st/serial"
 )
 
+var port serial.Port
+
 func initializeRfid() {
-	ports, err := serial.GetPortsList()
+	mode := &serial.Mode{
+		BaudRate: 115200,
+	}
+
+	port, err := serial.Open("/dev/ttyS3", mode)
 	if err != nil {
 		log.Fatal(err)
 	}
-	if len(ports) == 0 {
-		log.Fatal("No serial ports found!")
+
+	buff := make([]byte, 100)
+	for {
+		n, err := port.Read(buff)
+		if err != nil {
+			log.Fatal(err)
+			break
+		}
+		if n == 0 {
+			fmt.Println("\nEOF")
+			break
+		}
+		fmt.Printf("%v", string(buff[:n]))
 	}
-	for _, port := range ports {
-		fmt.Printf("Found port: %v\n", port)
-	}
+}
+
+func stopRfid() {
+	port.Write([]byte{0x5A, 0x00, 0x01, 0x01, 0x01, 0x00, 0x00, 0x00, 0xEB, 0xD5})
 }
